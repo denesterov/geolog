0a0c37234cca22146eb4ca24de04b2ab4ab312ce
Denis Nesterov <denis.nesterov@playrix.com>
2025-05-02 03:55:53 +0200

    Tracks selection menu and GPX download

--- geobot.py
+++ geobot.py
@@ -6,8 +6,10 @@ from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 from redis.commands.search.query import Query
 import uuid
 import time
+import datetime
 import telegram
 import telegram.ext
+import gpx
 
 
 # import redis.commands.search.aggregation as aggregations
@@ -61,6 +63,7 @@ def db_setup_redis():
             NumericField('usr_id'),
             NumericField('chat_id'),
             NumericField('msg_id'),
+            NumericField('ts'),
         )
         sess_index.create_index(
             sess_schema,
@@ -102,6 +105,11 @@ def db_get_session(usr_id, msg_id, tg_chat):
             'chat_type' : 'PRIV' if tg_chat.type == 'private' else 'PUB',
             'chat_name' : tg_chat.title if tg_chat.title is not None else (tg_chat.username if tg_chat.username is not None else '-'),
             'ts' : time.time(),
+            'length' : 0.0,
+            'duration' : 0.0,
+            'last_lat' : 0.0,
+            'last_long' : 0.0,
+            'last_update' : 0.0,
         }
         new_res = r.hset(uid, mapping=session)
         logger.info(f'New session. usr_id={usr_id}, uid={uid}, res={new_res}')
@@ -148,25 +156,101 @@ async def cmd_message(update: telegram.Update, context: telegram.ext.ContextType
         await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Your location updated. new={new_location}')
 
 
-async def cmd_trace(update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE):
+def db_escape_for_exact_search(hash_id):
+    escaped = hash_id.replace(':', '\\:').replace('-', '\\-')
+    return f'{{{escaped}}}'
+
+
+def sessions_menu_item(sess_id: str):
+    logger.info(f'sessions_menu_item. sess_id={sess_id}')
+    r = db_get_redis()
+    sess_data = r.hgetall(sess_id)
+    logger.info(f'sessions_menu_item. data={sess_data}')
+
+    point_idx = r.ft('idx:point')
+    pnt_res = point_idx.search(Query(f'@sess_id:{db_escape_for_exact_search(sess_id)}').dialect(2))
+    logger.info(f'sessions_menu_item. points={pnt_res.total}')
+
+    gpx_inst = gpx.GPX()
+    gpx_inst.name = 'Telegram GPS track'
+    gpx_inst.creator='Geograph'
+    gpx_inst.descr = 'This GPX file was created by Geograph Telegram Bot'
+
+    for pnt in pnt_res.docs:
+        wp = gpx.Waypoint()
+        wp.lat = float(pnt.latitude)
+        wp.lon = float(pnt.longitude)
+        wp.time = datetime.datetime.fromtimestamp(round(float(pnt.ts), 1))
+        gpx_inst.waypoints.append(wp)
+
+    sess_len = float(sess_data['length']) / 1000.0
+    sess_dur = float(sess_data['duration']) / 60.0
+    descr = f'Here is your GPX file\nLength {sess_len:.1f} km, duration {sess_dur:.0f} minutes, has {pnt_res.total} points'
+
+    sess_ts = float(sess_data['ts'])
+    sess_tm = datetime.datetime.fromtimestamp(sess_ts)
+    file_name = f'TelegramTrack_{sess_tm.year}{sess_tm.month:02}{sess_tm.day:02}_{sess_tm.hour:02}{sess_tm.minute:02}.gpx'
+
+    return (descr, telegram.InputFile(gpx_inst.to_string(), file_name))
+
+
+def sessions_menu_create(usr_id: int, offset: int, page: int):
+    logger.info(f'create_sessions_menu. offset={offset}, page={page}')
+
+    r = db_get_redis()
+    sess_idx = r.ft('idx:session')
+    sess_q = Query(f'@usr_id:[{usr_id} {usr_id}]').sort_by('ts', asc=False).paging(offset, page)
+    res = sess_idx.search(sess_q)
+    logger.info(f'create_sessions_menu. session data. len={res.total}')
+
+    keyboard = []
+    for doc in res.docs:
+        sess_id = doc.id
+        length = float(doc.length) / 1000.0
+        age = time.time() - float(doc.ts)
+        age_days = int(age / 86400)
+        age_hours = (age - age_days * 86400) / 3600
+        descr = f'Record {length:.1f} km, ' + (f'{age_days:.0f} days' if age_days > 0 else '') + f'{age_hours:.0f} hours ago'
+        keyboard.append([telegram.InlineKeyboardButton(descr, callback_data=f'session_menu_item {sess_id}')])
+
+    navig_butts = []
+    if offset >= page:
+        navig_butts.append(telegram.InlineKeyboardButton('Prev', callback_data=f'session_menu {offset - page} {page}'))
+    if len(res.docs) >= page:
+        navig_butts.append(telegram.InlineKeyboardButton('Next', callback_data=f'session_menu {offset + page} {page}'))
+    keyboard.append(navig_butts)
+
+    return (f'Records from {offset} to {min(offset + page, res.total)} of {res.total}', telegram.InlineKeyboardMarkup(keyboard))
+
+
+async def cmd_tracks(update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE):
     usr_id = update.effective_user.id
-    logger.info(f'Trace Chat Command. usr_id={usr_id}')
+    logger.info(f'cmd_tracks. usr_id={usr_id}')
+    menu_text, menu = sessions_menu_create(usr_id, 0, 3)
+    await update.message.reply_text(menu_text, reply_markup=menu)
+
+
+async def cmd_debug_tracks(update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE):
+    usr_id = update.effective_user.id
+    logger.info(f'cmd_debug_tracks. usr_id={usr_id}')
     r = db_get_redis()
     sess_idx = r.ft('idx:session')
     point_idx = r.ft('idx:point')
-    res = sess_idx.search(Query(f'@usr_id:[{usr_id} {usr_id}]'))
-    logger.info(f'Trace Chat Command. sessions_found={res.total}')
 
-    lines = [f'Hello {update.effective_user.first_name} here is your traces:\n\n']
+    sess_q = Query(f'@usr_id:[{usr_id} {usr_id}]').sort_by('ts', asc=False)
+    res = sess_idx.search(sess_q)
+    logger.info(f'cmd_debug_tracks. sessions_found={res.total}')
+
+    lines = [f'Hello {update.effective_user.first_name} here is your tracks:\n\n']
     for doc in res.docs:
         sess_id = doc.id
-        sess_id_tr = sess_id.replace(':', '\\:').replace('-', '\\-')
-        q = f'@sess_id:{{{sess_id_tr}}}'
-        logger.info(f'Trace Chat Command. query={q}')
+        q = f'@sess_id:{db_escape_for_exact_search(sess_id)}'
+        logger.info(f'cmd_debug_tracks. query={q}')
         pnt_res = point_idx.search(Query(q).dialect(2))
+        logger.info(f'cmd_debug_tracks. points={pnt_res.total}')
         ts = float(doc.ts)
         tm = time.asctime(time.gmtime(ts))
-        lines.append(f'Trace: {doc.chat_name}, {tm}, {pnt_res.total} points\nid {sess_id}\n\n')
+        lines.append(f'Track: {doc.chat_name}, {pnt_res.total} points\n{sess_id}\n{tm}\n\n')
     
     await context.bot.send_message(chat_id=update.effective_chat.id, text=''.join(lines))
 
@@ -181,29 +265,21 @@ def create_menu(start: int, span: int):
     logger.info(f'Creating menu: {start} - {span}')
     keyboard = []
     for i in range(start, start + span):
-        keyboard.append([telegram.InlineKeyboardButton(f'Option {fibo(i)}', callback_data=f'get {i}')])
+        keyboard.append([telegram.InlineKeyboardButton(f'Option {fibo(i)}', callback_data=f'test_menu_item {i}')])
 
     keyboard.append([
-        telegram.InlineKeyboardButton('Prev', callback_data=f'menu {start - span} {span}'),
-        telegram.InlineKeyboardButton('Next', callback_data=f'menu {start + span} {span}'),
+        telegram.InlineKeyboardButton('Prev', callback_data=f'test_menu {start - span} {span}'),
+        telegram.InlineKeyboardButton('Next', callback_data=f'test_menu {start + span} {span}'),
     ])
 
     return telegram.InlineKeyboardMarkup(keyboard)
 
-async def cmd_test_menu(update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE):
-    #  keyboard = [
-    #    [
-    #         telegram.InlineKeyboardButton("Option 1", callback_data="1"),
-    #         telegram.InlineKeyboardButton("Option 2", callback_data="2"),
-    #     ],
-    #     [telegram.InlineKeyboardButton("Option 3", callback_data="3")],
-    # ]
-    # reply_markup = telegram.InlineKeyboardMarkup(keyboard)
 
+async def cmd_test_menu(update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Your menu', reply_markup=create_menu(0, 10))
 
 
-async def cmd_test_button(update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE) -> None:
+async def cmd_button(update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
 
     # CallbackQueries need to be answered, even if no notification to the user is needed
@@ -211,14 +287,23 @@ async def cmd_test_button(update: telegram.Update, context: telegram.ext.Context
     await query.answer()
 
     data = query.data.split(' ')
-    if data[0] == 'get':
+    if data[0] == 'test_menu_item':
         num = int(data[1])
         await query.edit_message_text(text=f'You selected number {num}')
         await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Your number is {fibo(num)}')
-    elif data[0] == 'menu':
+    elif data[0] == 'test_menu':
         fromm = int(data[1])
         to = int(data[2])
         await query.edit_message_text(text='Your menu', reply_markup=create_menu(fromm, to))
+    elif data[0] == 'session_menu_item':
+        descr, gpx_file = sessions_menu_item(data[1])
+        await query.edit_message_text(text=descr)
+        await context.bot.send_document(update.effective_chat.id, gpx_file)
+    elif data[0] == 'session_menu':
+        usr_id = update.effective_user.id
+        logger.info(f'cmd_button. session_menu. usr_id={usr_id}')
+        menu_text, menu = sessions_menu_create(usr_id, int(data[1]), int(data[2]))
+        await query.edit_message_text(text=menu_text, reply_markup=menu)
     else:
         await query.edit_message_text(text='Wrong menu button')
 
@@ -242,9 +327,10 @@ def mainloop():
 
     application = telegram.ext.ApplicationBuilder().token(BOT_TOKEN).build()
 
-    application.add_handler(telegram.ext.CommandHandler('trace', cmd_trace))
+    application.add_handler(telegram.ext.CommandHandler('tracks', cmd_tracks))
+    application.add_handler(telegram.ext.CommandHandler('debug_tracks', cmd_debug_tracks))
     application.add_handler(telegram.ext.CommandHandler('test_menu', cmd_test_menu))
-    application.add_handler(telegram.ext.CallbackQueryHandler(cmd_test_button))
+    application.add_handler(telegram.ext.CallbackQueryHandler(cmd_button))
     application.add_handler(telegram.ext.MessageHandler(telegram.ext.filters.LOCATION & (~telegram.ext.filters.COMMAND), cmd_message))
 
     application.run_polling()
