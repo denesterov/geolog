a6a0ee06c4353ae5efd8dc1b01123e9ee2c97d81
Denis <copypastor@gmail.com>
2025-05-12 19:43:48 +0200

    Map improved

--- src/const.py
+++ src/const.py
@@ -3,3 +3,9 @@ MIN_GEO_DELTA = 25.0 # Minimal delta in meters for next track point to be record
 MAX_SPEED = 10.0 # Maximum speed, m/s
 AFTER_PAUSE_TIME = 180.0 # Timeout after wich track is paused, if there is no movement 
 DEFAULT_BASE_DIR = './geolog-bot-images' # Default map images path
+
+MIN_POINTS_FOR_MAP = 10 # Minimal points num to generate a map
+MIN_ANGULAR_SIZE_FOR_MAP = 0.002 # Minimal angular size of track to have a map
+MAP_ANGULAR_SIZE_THRESHOLD = 0.005 # Threshold of hi/lo map detail
+MAP_LO_DETAIL_LVL = 14
+MAP_HI_DETAIL_LVL = 16
--- src/maps.py
+++ src/maps.py
@@ -27,7 +27,10 @@ async def try_create_map():
         return
     
     logger.info(f'try_create_map. Creating map for track. sess_id={sess_id}')
-    _, segments = db.get_track(sess_id)
+    track_info, segments = db.get_track(sess_id)
+    # if track_info.points_total < const.MIN_POINTS_FOR_MAP:
+    #     db.finish_map_job(sess_id)
+    #     return
 
     base_path = os.environ.get('MAP_IMAGES_DIR', const.DEFAULT_BASE_DIR)
     os.makedirs(base_path, exist_ok=True)
@@ -50,12 +53,15 @@ async def try_create_map():
 
     lat_delta = lat_max - lat_min
     lon_delta = lon_max - lon_min
-    MIN_ANG_SIZE = 0.002 # it is approx 200 meters in europe
+    lowest_ang_delta = min(lat_delta, lon_delta)
+    # if lowest_ang_delta < const.MIN_ANGULAR_SIZE_FOR_MAP:
+    #    db.finish_map_job(sess_id)
+    #    return
+
     aspect = 1.0
-    if lat_delta > MIN_ANG_SIZE and lon_delta > MIN_ANG_SIZE:
-        aspect = lon_delta / lat_delta
-        aspect = max(aspect, 0.33)
-        aspect = min(aspect, 3.0)
+    aspect = lon_delta / lat_delta
+    aspect = max(aspect, 0.33)
+    aspect = min(aspect, 3.0)
 
     BASE_FIG_SIZE = 10.0
     fig_size = None
@@ -76,7 +82,8 @@ async def try_create_map():
 
     ax.set_extent([lon_min, lon_max, lat_min, lat_max], crs=ccrs.PlateCarree())
 
-    ax.add_image(tiler, 14)  # число 14 — уровень масштабирования (чем больше, тем детальнее)
+    detail_lvl = const.MAP_LO_DETAIL_LVL if lowest_ang_delta < const.MAP_ANGULAR_SIZE_THRESHOLD else const.MAP_HI_DETAIL_LVL
+    ax.add_image(tiler, detail_lvl)
 
     for points in segments:
         lons = []
@@ -90,6 +97,7 @@ async def try_create_map():
     fname = get_filename(sess_id) 
     plt.savefig(fname, dpi=300, format='jpg', bbox_inches='tight', pad_inches=0)
     logger.info(f'try_create_map. saved. sess_id={sess_id}, filename={fname}')
+
     db.finish_map_job(sess_id)
 
 
