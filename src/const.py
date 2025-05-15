73ed166ffef32afcb5416ca8f71745591137f19d
Denis <copypastor@gmail.com>
2025-05-15 16:26:30 +0200

    Adjust speed constants

--- src/const.py
+++ src/const.py
@@ -1,6 +1,6 @@
 
-MIN_GEO_DELTA = 25.0 # Minimal delta in meters for next track point to be recorded
-MAX_SPEED = 10.0 # Maximum speed, m/s
+MIN_GEO_DELTA = 30.0 # Minimal delta in meters for next track point to be recorded
+MAX_SPEED = 16.0 # Maximum speed, m/s
 AFTER_PAUSE_TIME = 180.0 # Timeout after wich track is paused, if there is no movement 
 DEFAULT_BASE_DIR = './geolog-bot-images' # Default map images path
 
