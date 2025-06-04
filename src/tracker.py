bbb5081c425c1c89794fb3b1faf09b18047a70cc
Denis <copypastor@gmail.com>
2025-06-04 19:29:36 +0300

    Basic operations and tests

--- src/tracker.py
+++ src/tracker.py
@@ -1,47 +1,49 @@
 import telegram
+from collections import namedtuple
 
-class DbAdapter:
-    def __init__(self, session_data: dict):
-        pass
-
-    def get_session_data(self, session_id: str):
-        pass
-
+Point = namedtuple('Point', ['lat', 'lon', 'timestamp'])
 
 class SessionData:
-    #__slots__ = ["session_data", "dirty"]
+    valid_data_fields = {
+        'usr_id', 'chat_id', 'msg_id', 'chat_type', 'chat_name',
+        'ts', 'length', 'duration', 'last_update',
+        'last_lat', 'last_long', 'track_segm_idx', 'track_segm_len' }
+
     def __init__(self, session_data: dict):
         self.__dict__['session_data'] = session_data
         self.__dict__['dirty'] = set()
 
-    def __getattr__(self, item):
+    def __getattr__(self, name):
         sess_data = self.__dict__['session_data']
-        if item in sess_data:
-            return sess_data[item]
-        raise AttributeError(f"SessionData object has no attribute '{item}'")
+        valid_data_fields = SessionData.__dict__['valid_data_fields']
+        assert name in valid_data_fields
+        if name in sess_data:
+            return sess_data[name]
+        raise AttributeError(f"SessionData object has no attribute '{name}'")
 
-    def __setattr__(self, item, value):
+    def __setattr__(self, name, value):
         sess_data = self.__dict__['session_data']
         dirty = self.__dict__['dirty']
-        sess_data[item] = value
-        dirty.add(item)
+        sess_data[name] = value
+        dirty.add(name)
 
-    def get_dirty(self):
+    def get_dirty_fields(self):
         return self.__dict__['dirty']
 
 
+class PointsData:
+    def __init__(self):
+        self.points = []
+
+    def add_point(self, point: Point):
+        self.points.append(point)
+
+
 class Tracker:
-    def __init__(self, session_data: dict):
+    def __init__(self, session_data: SessionData, points: PointsData):
         self.session_data = session_data
+        self.points = points
 
     def add_location(self, location: telegram.Location, new_location: bool = False):
 
-        pass
-
-
-class Tracker:
-    def __init__(self, session_id: str):
-        self.session_id = session_id
-
-    def add_location(self, location: telegram.Location):
         pass
--- test/unit/test_session_data.py
+++ test/unit/test_session_data.py
@@ -1,42 +1,40 @@
 import pytest
-from tracker import SessionData
+import tracker
 
 
 def test_session_data_init():
-    test_data = {'name': 'test', 'value': 123}
-    sess = SessionData(test_data)
-    assert sess.__dict__['session_data']['name'] == 'test'
-    assert sess.__dict__['session_data']['value'] == 123
+    test_data = {'chat_name': 'test', 'usr_id': 123}
+    sess = tracker.SessionData(test_data)
+    assert sess.__dict__['session_data']['chat_name'] == 'test'
+    assert sess.__dict__['session_data']['usr_id'] == 123
     assert sess.__dict__['dirty'] == set()
 
 
 def test_session_data_get():
-    test_data = {'name': 'test', 'value': 123}
-    sess = SessionData(test_data)
-    assert sess.name == 'test'
-    assert sess.value == 123
+    test_data = {'chat_name': 'test', 'usr_id': 123}
+    sess = tracker.SessionData(test_data)
+    assert sess.chat_name == 'test'
+    assert sess.usr_id == 123
 
 
 def test_session_data_set():
-    test_data = {'name': 'test'}
-    sess = SessionData(test_data)
-    assert sess.name == 'test'
-    sess.name = 'abc'
-    assert sess.name == 'abc'
-    assert sess.get_dirty() == {'name'}
+    test_data = {'chat_name': 'test'}
+    sess = tracker.SessionData(test_data)
+    assert sess.chat_name == 'test'
+    sess.chat_name = 'abc'
+    assert sess.chat_name == 'abc'
+    assert sess.get_dirty_fields() == {'chat_name'}
 
 
-@pytest.mark.skip(reason="no way of currently testing this")
-def test_session_data_getattr():
-    test_data = {'name': 'test', 'value': 123}
-    session = SessionData(test_data)
-    assert session.name == 'test'
-    assert session.value == 123
-
-
-@pytest.mark.skip(reason="no way of currently testing this")
 def test_session_data_invalid_attribute():
-    test_data = {'name': 'test'}
-    session = SessionData(test_data)
-    with pytest.raises(KeyError):
+    test_data = {'chat_name': 'test'}
+    session = tracker.SessionData(test_data)
+    with pytest.raises(AssertionError):
         _ = session.nonexistent
+
+
+def test_points_data():
+    data = tracker.PointsData()
+    assert data.points == []
+    data.add_point(tracker.Point(33.44, 55.66, 1749053938))
+    assert data.points == [tracker.Point(33.44, 55.66, 1749053938)]
