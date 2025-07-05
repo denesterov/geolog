08ef07f62b5a9d4cb18af28a90c6563c97388f13
Denis <copypastor@gmail.com>
2025-07-05 23:15:52 +0300

    Unit tests from general cases

--- test/unit/test_tracker.py
+++ test/unit/test_tracker.py
@@ -19,19 +19,4 @@ def test_new_session():
 
 
 def test_smoke():
-    c = cases_data.smoke
-
-    tr, sd, pd = tracker_test_utils.create_basic_env(c.track[0][0])
-
-    tr.update(c.track[0][0], location_is_new=True)
-    tr.update(c.track[0][1])
-    tr.update(c.track[0][2])
-
-    assert sd.get_dirty_fields() == {'track_segm_len', 'last_lat', 'last_long', 'last_update', 'length', 'duration'}
-    print(f'SESSION LENGTH={sd.length}, DURATION={sd.duration}')
-    assert sd.length == pytest.approx(156.0 + 74.7, 1.0)
-    assert sd.duration == 70.0
-    assert len(pd.points) == 3
-    assert pd.points[0] == c.track[0][0]
-    assert pd.points[1] == c.track[0][1]
-    assert pd.points[2] == c.track[0][2]
+    tracker_test_utils.help_test_track(cases_data.smoke)
--- test/unit/tracker_test_utils.py
+++ test/unit/tracker_test_utils.py
@@ -1,7 +1,7 @@
 import pytest
 import tracker
 import common
-
+import cases_data
 
 def get_default_sess_data(first_point:common.Point=None):
     fp = first_point if first_point is not None else common.Point(43.776122, 21.893356, 1749054111)
@@ -16,3 +16,29 @@ def create_basic_env(first_point:common.Point=None, sess_data=None):
     pd = tracker.PointsData()
     tr = tracker.Tracker(sd, pd)
     return (tr, sd, pd)
+
+
+def help_test_track(cs: cases_data.Case):
+    tr, sd, pd = create_basic_env(cs.track[0][0])
+
+    for segm in cs.track:
+        for pnt in segm:
+            tr.update(pnt, location_is_new=pnt is cs.track[0][0])
+
+    assert sd.get_dirty_fields() == {'track_segm_len', 'last_lat', 'last_long', 'last_update', 'length', 'duration'}
+    assert sd.length == pytest.approx(cs.expect_length, 1.0)
+    assert sd.duration == pytest.approx(cs.expect_duration, 0.1)
+    assert len(pd.points) == cs.expect_gpx_points
+
+    filtered_points = []
+    for i, seg in enumerate(cs.track):
+        if i in cs.skip_segments:
+            continue
+        filtered_points += [point for j, point in enumerate(seg) if (i, j) not in cs.skip_points]
+
+    print(f'help_test_track. {pd.points=}')
+    print(f'help_test_track. {filtered_points=}')
+    assert len(pd.points) == len(filtered_points)
+
+    for i, pnt in enumerate(filtered_points):
+        assert pd.points[i] == pnt
